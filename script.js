99dd6c58e52d0b993a8fcbd06f458fbc267bc85a
Moktar Mohamed <saraisamo123@gmail.com>
2022-11-22 11:25:14 +0300

    code update- check-icon js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var check = document.querySelector(".check-icon");
 var checked = document.querySelector(".checked-icon");
-var todoText = document.querySelector(".todo-text").innerHTML;
+var todoText = document.querySelector(".todo-text");
 
+// the ckeck icon code
 check.addEventListener("click", function (e) {
   e.preventDefault();
   document.querySelector(".check-icon").style.display = "none";
   document.querySelector(".checked-icon").style.display = "block";
-  document.querySelector(".todo-text").textContent = todoText;
+  todoText.outerHTML = "<del>" + todoText.innerHTML + "</del>";
 });
 
+// the ckecked icon code
 checked.addEventListener("click", function (e) {
   e.preventDefault();
   document.querySelector(".check-icon").style.display = "block";
   document.querySelector(".checked-icon").style.display = "none";
+  todoText.outerHTML = "<p>" + todoText.innerHTML + "</p>";
 });
