0294035b7a45a214723b2e0efba5bcdeef998479
Moktar Mohamed <saraisamo123@gmail.com>
2022-11-23 13:08:16 +0300

    update

--- script.js
+++ script.js
@@ -2,18 +2,55 @@ var check = document.querySelector(".check-icon");
 var checked = document.querySelector(".checked-icon");
 var todoText = document.querySelector(".todo-text");
 
-// the ckeck icon code
-check.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.querySelector(".check-icon").style.display = "none";
-  document.querySelector(".checked-icon").style.display = "block";
-  todoText.outerHTML = "<del>" + todoText.innerHTML + "</del>";
-});
+//! the ckeck icon code     personal-note(use this when you are using icon)
 
-// the ckecked icon code
-checked.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.querySelector(".check-icon").style.display = "block";
-  document.querySelector(".checked-icon").style.display = "none";
-  todoText.outerHTML = "<p>" + todoText.innerHTML + "</p>";
-});
+// check.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   document.querySelector(".check-icon").style.display = "none";
+//   document.querySelector(".checked-icon").style.display = "block";
+//   check.parentElement.classList.add("line-through");
+// });
+
+//! the ckecked icon code     personal-note(use this when you are using icon)
+// checked.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   document.querySelector(".check-icon").style.display = "block";
+//   document.querySelector(".checked-icon").style.display = "none";
+//   check.parentElement.classList.remove("line-through");
+// });
+
+// this the javascript code for the system
+
+// add todos list
+function addTodosList() {
+  var input = document.querySelector(".todo-input");
+  if (input.value == "") {
+    alert("empty");
+  } else {
+    var todos = {
+      todo: input.value,
+      status: "undone",
+    };
+
+    var json = JSON.stringify(todos);
+    console.log(typeof json);
+    var data = [];
+    data = JSON.parse(localStorage.getItem("todo"));
+    data.push(json);
+    console.log(data);
+    localStorage.setItem("key", JSON.stringify(data));
+  }
+}
+
+// show todos list
+
+// updating todos list
+function changeStatus(check) {
+  if (check.checked) {
+    check.parentElement.classList.add("line-through");
+  } else {
+    check.parentElement.classList.remove("line-through");
+  }
+}
+
+// delete todos list
